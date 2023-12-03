270ce89b5ad3bd3ff4854bc5d4ff8df025a50514
Zafer Yıldız <zafervrcic@gmail.com>
2023-12-04 02:09:39 +0300

    Update script.js
    js up

--- script.js
+++ script.js
@@ -8,7 +8,7 @@ arrows.forEach((arrow, i) => {
   const imageItem = movieLists[i].querySelectorAll("img").length;
   arrow.addEventListener("click", function () {
     clickCounter++;
-    if (imageItem - (4 + clickCounter) + (4 - widthRatio) >= 0) {
+    if (imageItem - (3 + clickCounter) + (3 - widthRatio) >= 0) {
       movieLists[i].style.transform = `translateX(${
         movieLists[i].computedStyleMap().get("transform")[0].x.value - 300
       }px)`;
